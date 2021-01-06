a3746da50f6a3f3a5ecccf29e0645b535dd34a12
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-01-06 15:38:46 -0600

    Add attributes and empty methods to activity class

--- activity.js
+++ activity.js
@@ -1,1 +1,25 @@
+class Activity {
+  constructor(obj){
+    this.category = obj.category;
+    this.description = obj.description;
+    this.minutes = obj.minutes;
+    this.seconds = obj.seconds;
+    this.completed = false;
+    this.id = Date.now();
+  }
 
+  countdown() {
+
+  }
+
+  markComplete() {
+
+  }
+
+  saveToStorage() {
+    
+  }
+
+}
+
+module.exports = Activity;
